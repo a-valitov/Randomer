354f2e27ec459310d409a9e7e130f8f21bebc7a3
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-02-16 12:29:10 +0500

    Basic functionality added

--- app/src/main/java/com/avalitov/randomer/MainActivity.kt
+++ app/src/main/java/com/avalitov/randomer/MainActivity.kt
@@ -2,10 +2,36 @@ package com.avalitov.randomer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var getRandomButton: Button
+    internal lateinit var randomNumberTextView: TextView
+
+//    companion object Default : Random() {
+//        override fun nextBits(bitCount: Int): Int {
+//            TODO("Not yet implemented")
+//        }
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getRandomButton = findViewById(R.id.getRandomButton)
+        randomNumberTextView = findViewById(R.id.randomNumberTextView)
+
+        getRandomButton.setOnClickListener() { view ->
+            getNewRandom()
+        }
     }
+
+    private fun getNewRandom() {
+        var random = Random.nextInt(1,101)
+        randomNumberTextView.text = random.toString()
+    }
+
 }
