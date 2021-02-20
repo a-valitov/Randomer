7f55d1214603d96684fc803574e104305ff23618
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-02-20 14:46:53 +0500

    RandomGenerator class added (and this time I mean it)

---
+++ app/src/main/java/com/avalitov/randomer/RandomGenerator.kt
@@ -0,0 +1,12 @@
+package com.avalitov.randomer
+
+import kotlin.random.Random
+
+class RandomGenerator {
+
+    fun getNewRandom(fromNumber: Int, untilNumber: Int):Int {
+        return if (fromNumber > untilNumber) -1 else Random.nextInt(fromNumber, untilNumber)
+        // this is probably not what you meant, is it?
+    }
+
+}
